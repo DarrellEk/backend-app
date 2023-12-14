99cc40bb2245e46a0c077a4b8c1b0c98a1167414
ischo <ischo@LAPTOP-9GOA4UHJ.localdomain>
2023-12-14 16:02:23 +0800

    have to test with auth token from sign in

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express"
+import uploadImageRouter from "./src/controllers/uploadImage.controller.js"
 
 
 const app = express()
 app.use(express.json())
+app.use('/upload-Image', uploadImageRouter)
 
 
 export default app
