4e668a14e0b482c32cf04d51efc52da2d879e0bb
ischo <ischo@LAPTOP-9GOA4UHJ.localdomain>
2023-12-20 11:40:11 +0800

    added get memory by id and get memories by user

--- src/controllers/memories.controller.js
+++ src/controllers/memories.controller.js
@@ -21,6 +21,129 @@ memoriesRouter.use(cors());
 //     userId        Int
 //   }
 
+memoriesRouter.get('/:id',auth, async(req, res)=>{
+  //selects a memory by id, and returns all associated images and videos as well
+  const memoryId = parseInt(req.params.id)
+  let memory = []
+  try{
+  memory = await prisma.memories.findUnique({
+    where:{
+      id: memoryId
+    },
+    include:{
+      images:{},
+      videos:{}
+    }
+  })
+
+  if(memory===null||Object.keys(memory).length === 0){
+    res.status(404).send({error: "Could not find requested memory"})
+  }else{
+    res.json(memory)
+  }
+}catch(error){
+  console.log("error:", error.message);
+  res.status(500).send({error: "Error fetching memory"})
+}
+
+  //example response:
+  /*
+  {
+	"id": 4,
+	"location": "1.45634235, 196.4352",
+	"time": "2023-12-18T07:04:18.911Z",
+	"description": "des",
+	"title": "title",
+	"created_at": "2023-12-20T03:13:29.446Z",
+	"updated_at": "2023-12-20T03:13:29.446Z",
+	"userId": 1,
+	"images": [
+		{
+			"id": 3,
+			"url": "https://img4.com",
+			"description": "description",
+			"memoryId": 4,
+			"created_at": "2023-12-20T03:14:27.567Z",
+			"updated_at": "2023-12-20T03:14:27.567Z"
+		},
+		{
+			"id": 4,
+			"url": "https://img5.com",
+			"description": "description",
+			"memoryId": 4,
+			"created_at": "2023-12-20T03:14:33.627Z",
+			"updated_at": "2023-12-20T03:14:33.627Z"
+		}
+	],
+	"videos": [
+		{
+			"id": 7,
+			"url": "https://example.com/video4.mp4",
+			"description": "video description6",
+			"memoryId": 4,
+			"created_at": "2023-12-20T03:13:58.603Z",
+			"updated_at": "2023-12-20T03:13:58.603Z"
+		},
+		{
+			"id": 8,
+			"url": "https://example.com/video5.mp4",
+			"description": "video description6",
+			"memoryId": 4,
+			"created_at": "2023-12-20T03:14:08.246Z",
+			"updated_at": "2023-12-20T03:14:08.246Z"
+		}
+	]
+}
+  */
+})
+
+memoriesRouter.get('/user/:userId', auth, async (req, res)=>{
+    const userId = parseInt(req.params.userId)
+
+    //selects all memories belonging to a user. does not return any images or videos 
+    try{
+    const userMemories = await prisma.memories.findMany({
+      where:{
+        userId: userId
+      }
+    })
+
+    if(userMemories===null||Object.keys(userMemories).length === 0){
+      res.status(404).send({error: "Could not find any memories for this user, or user does not exist"})
+    }else{
+      res.json(userMemories)
+    }
+    }catch(error){
+      console.log("error:", error.message);
+      res.status(500).send({error: "Error fetching memories"})
+    }
+    //example response:
+    /*
+    [
+	{
+		"id": 1,
+		"location": "1.45634235, 196.4352",
+		"time": "2023-12-18T07:04:18.911Z",
+		"description": "description",
+		"title": "title",
+		"created_at": "2023-12-18T07:08:39.924Z",
+		"updated_at": "2023-12-18T07:08:39.924Z",
+		"userId": 1
+	},
+	{
+		"id": 2,
+		"location": "1.45634235, 196.4352",
+		"time": "2023-12-18T07:04:18.911Z",
+		"description": "wet",
+		"title": "surfing",
+		"created_at": "2023-12-18T07:11:09.038Z",
+		"updated_at": "2023-12-18T07:12:57.936Z",
+		"userId": 1
+	}
+]
+    */
+})
+
 memoriesRouter.post('/', auth, async (req,res)=>{
     const data = req.body
 
